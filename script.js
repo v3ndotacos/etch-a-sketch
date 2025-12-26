c92d32bfbcc4e3665b77949b78715925ae0fe3be
n.mbre <149480076+v3ndotacos@users.noreply.github.com>
2025-12-26 22:24:54 +0100

    extras, random and opacity for each square

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-const DEFAULT = 16;
-const LIMIT = 100;
+const DEFAULT_SIZE = 16;
+const LIMIT_INPUT = 100;
+const MAX_RGB = 256;
+
+function randomColor() {
+  let R = Math.floor(Math.random() * MAX_RGB);
+  let G = Math.floor(Math.random() * MAX_RGB);
+  let B = Math.floor(Math.random() * MAX_RGB);
+  let color = "rgb(" + R + "," + G + "," + B + ")";
+  return color;
+}
 
 function createGrid(container, size) {
   let i;
+  let currentOp;
   const total = size * size;
 
   while (container.firstChild) {
@@ -14,11 +24,13 @@ function createGrid(container, size) {
     cuadrado.classList.add("grid");
     cuadrado.classList.add("v" + i);
     cuadrado.style.flex = `0 0 calc(100% / ${size})`;
+    cuadrado.style.opacity = 0;
 
-    cuadrado.addEventListener(
-      "mouseover",
-      () => (cuadrado.style.backgroundColor = "black")
-    );
+    cuadrado.addEventListener("mouseover", () => {
+      cuadrado.style.backgroundColor = randomColor();
+      currentOp = +cuadrado.style.opacity;
+      cuadrado.style.opacity = currentOp + 0.1;
+    });
     container.appendChild(cuadrado);
   }
 }
@@ -32,7 +44,7 @@ function main() {
   askButton.classList.add("ask-button");
 
   btnContainer.appendChild(askButton);
-  createGrid(container, DEFAULT);
+  createGrid(container, DEFAULT_SIZE);
 
   askButton.addEventListener("click", promptMe);
 
@@ -41,7 +53,7 @@ function main() {
     do {
       userSize = prompt("Please provide an grid Size");
       alert(userSize);
-    } while (userSize > LIMIT);
+    } while (userSize > LIMIT_INPUT);
     createGrid(container, +userSize);
   }
 }
