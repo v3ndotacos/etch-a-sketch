4544a77f2d5ec861cddd55f9dea3a981d9b7479a
n.mbre <149480076+v3ndotacos@users.noreply.github.com>
2025-12-26 17:12:34 +0100

    js creategrid method

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+function createGrid(container) {
+  let i;
 
-function main(){
-    
+  for (i = 0; i < 256; i++) {
+    const cuadrado = document.createElement("div");
+    cuadrado.classList.add("grid");
+    cuadrado.classList.add("v" + i);
 
-
-
+    container.appendChild(cuadrado);
+  }
 }
 
+function hoverGrid(container) {
+  //hover a grid and changes the background color
+  //use javascript
+  const trail = document.q
+}
 
+function main() {
+  const container = document.querySelector("#container");
+  createGrid(container);
+  hoverGrid(container);
 
+  //promtea unapregunta que cuantos grids quieres.
+  //luego se lo metes y con text input guarda el valor y lo pasa
+  // a la funcnion creargrid Multiplicar valor ^2.
+}
 
 main();
